afbaaa132bf0d41e6fb08019ed3e76dcdf5b23e4
Ethan <ethanfree23@gmail.com>
2026-03-21 22:13:00 -0500

    Fix CORS for techflash.app and vercel subdomains

--- skilled_hub_api/config/initializers/cors.rb
+++ skilled_hub_api/config/initializers/cors.rb
@@ -1,10 +1,14 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:5173', 'https://techflash.app', 'https://www.techflash.app'
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        expose: ['Authorization']
-    end
+  allow do
+    origins 'http://localhost:5173',
+            'https://techflash.app',
+            'https://www.techflash.app',
+            %r{https://.*\.techflash\.app},
+            %r{https://.*\.vercel\.app}
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['Authorization']
   end
+end
   
