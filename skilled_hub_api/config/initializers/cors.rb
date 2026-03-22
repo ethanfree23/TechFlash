e9b63db104cde9ccca7554f00e27bbdd48e4263f
Ethan <ethanfree23@gmail.com>
2026-03-21 22:15:18 -0500

    CORS: allow all origins temporarily

--- skilled_hub_api/config/initializers/cors.rb
+++ skilled_hub_api/config/initializers/cors.rb
@@ -1,10 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173',
-            'https://techflash.app',
-            'https://www.techflash.app',
-            %r{https://.*\.techflash\.app},
-            %r{https://.*\.vercel\.app}
+    origins '*'  # Allow all origins for now - tighten to techflash.app in production
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
